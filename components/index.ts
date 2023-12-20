0730d8596a9c104c1b8b25b067e9687267d133c1
negyeliczkysandor <sandor.negyeliczky@gmail.com>
2023-12-20 13:01:19 +0100

    add: fetch rate

---
+++ components/index.ts
@@ -0,0 +1,11 @@
+import ScreenHeaderBtn from "./common/header/ScreenHeaderBtn";
+
+// home screen
+import Welcome from "./home/welcome/Welcome";
+// job details screen
+
+
+export {
+    ScreenHeaderBtn,
+    Welcome,
+};
